426dab13bc4ad399dd02039915f700117dbe750d
BearSyrup <michaelc.araujo@gmail.com>
2019-02-13 01:27:02 -0200

    template knn

--- knn.py
+++ knn.py
@@ -1,7 +1,8 @@
 class KNN:
     def __init__(self, neighbors):
         self.neighbors = neighbors
-    def fit:
+
+    def fit():
         return "Not implemented"
     def predict():
         return "Not Implemented"
